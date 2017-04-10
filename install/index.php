91a14a2391b9a101a6ffebd98ea9ef8bd1979823
alpha <liuhaonan2003@gmail.com>
2017-04-10 14:21:38 +0800

    update

--- install/index.php
+++ install/index.php
@@ -15,14 +15,15 @@ if (!in_array($mod, $arr_mod)) { //非法调用
     exit("Access Denied");
 }
 
+#$base = $_SERVER["DOCUMENT_ROOT"] . str_ireplace(basename(dirname($_SERVER["PHP_SELF"])), "", dirname($_SERVER["PHP_SELF"]));
 $base = $_SERVER["DOCUMENT_ROOT"] . str_ireplace(basename(dirname($_SERVER["PHP_SELF"])), "", dirname($_SERVER["PHP_SELF"]));
 
-require($base . "config/config.class.php"); //载入初始化类
+require_once($base . "config/config.class.php"); //载入初始化类
 
 $obj_config = new CLASS_CONFIG(); //配置初始化
-
+//var_dump($obj_config);exit;
 $obj_config->config_gen(); //生成配置文件
 
-require($obj_config->str_pathRoot . "config/config.inc.php"); //载入配置
+require_once($obj_config->str_pathRoot . "config/config.inc.php"); //载入配置
 
-require(BG_PATH_MODULE . "install/ui/" . $mod . ".php"); //调用模块
+require_once(BG_PATH_MODULE . "install/ui/" . $mod . ".php"); //调用模块
